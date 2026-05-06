9269516d9eb6de056867c0df8769a278faa84f18
Huan Nguyen <huan.nguyen@intellisyncdata.com>
2026-05-06 22:45:42 +0700

    Revert 19 to 18

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ISD API Builder',
-    'version': '19.0.0.0.0',
+    'version': '18.0.1.0.0',
     'category': 'ISD Modules',
     'summary': 'AWS PostgreSQL Connection + DB Schema Browser + API Builder',
     'description': """
